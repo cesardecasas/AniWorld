06df44aa4a653f2237e9b8a2388679a604aa7bde
Cesar De Casas <cesar.decasasgarcia@gmail.com>
2021-09-07 10:48:17 -0500

    initial set up

---
+++ client/components/nav.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Nav = ()=>{
+
+
+    return(
+        <nav class="navbar navbar-dark bg-dark">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav
